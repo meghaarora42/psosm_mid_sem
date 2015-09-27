409dfcdf25edd31420e258533e87e1b8a40824cb
rohan katyal <rohan12086@iiitd.ac.in>
2015-09-27 11:40:26 +0530

    twitter data collection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,19 +13,22 @@ class ApplicationController < ActionController::Base
   	@keyword = params[:keyword_search]
   	puts @keyword
   	
-  	client = Twitter::Streaming::Client.new do |config|
+	client = Twitter::REST::Client.new do |config|
   		config.consumer_key        = "LQTGcZzXLxxpEQMeqdJ9aZJcu"
   		config.consumer_secret     = "i7auD1jyHCkkdy4y3mpyCzfS6PVqiu98ZDmEF7oO9JJr4C6642"
   		config.access_token        = "180749030-HLsI4GuxY5H5BnI37MgxecWg56ypORZG7g2nyPxU"
   		config.access_token_secret = "AYS3dCBNchYq5mKDfqaOppcAxOQwcUf089CDmZvOANwZT"
 	end
 
-	# redirect_to(action:'home')
-
-	client.filter(track: @keyword) do |object|
-  		puts object.text if object.is_a?(Twitter::Tweet)
+	client.search(@keyword).each do |object|
+  		puts object.text 
 	end
+
+	redirect_to(:action => 'home')
 
   end
 
+
+
 end
+
