2f3e41c0ff244c6ef88dacc2e8d0626d991f3cfc
Megha <megha893.arora@gmail.com>
2015-09-27 10:46:14 +0530

    db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  @keyword = ''
 
+  def home
+  end
 
   def twitter_data_collection
   	
-  	puts params[:keyword_search]
-  	
-  	@keyword = "police"
+  	@keyword = params[:keyword_search]
+  	puts @keyword
   	
   	client = Twitter::Streaming::Client.new do |config|
   		config.consumer_key        = "LQTGcZzXLxxpEQMeqdJ9aZJcu"
@@ -18,7 +20,9 @@ class ApplicationController < ActionController::Base
   		config.access_token_secret = "AYS3dCBNchYq5mKDfqaOppcAxOQwcUf089CDmZvOANwZT"
 	end
 
-	client.filter(track: @keyword do |object|
+	# redirect_to(action:'home')
+
+	client.filter(track: @keyword) do |object|
   		puts object.text if object.is_a?(Twitter::Tweet)
 	end
 
